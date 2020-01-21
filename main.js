53886231e77db48281583d3f761a78c0b148d2fe
MysticSeagull <Justinborzi@gmail.com>
2020-01-21 12:32:35 -0500

    Temp code for later use

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 var JSLib = require('./lib/app.js');
+const request = require('request');
+
+//npm install request@2.81.0
+
+request('https://random-word-api.herokuapp.com/word?key=KJ2E33RA&number=1', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body);
+});
 
 // Gets NPM Packages then finds the github.
 JSLib.getLib("browserify").then((val) => {
